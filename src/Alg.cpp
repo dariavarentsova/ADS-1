db4c55dadd002173fbbcf2b96eae2a93e99de0ac
dariavarentsova <63418397+dariavarentsova@users.noreply.github.com>
2020-05-18 00:20:33 +0300

    Update Alg.cpp

--- src/Alg.cpp
+++ src/Alg.cpp
@@ -7,11 +7,9 @@ int cbinsearch(int *arr, int size, int value)
 		c = (a + s)/2;
 		if (arr[c] > value)
 			s = c - 1;
-		else 
-    if (arr[c] < value)
+		else if (arr[c] < value)
 			a = c + 1;
-		else
-    el++;
+		else el++;
 	}
   
 	if (el == 1)
